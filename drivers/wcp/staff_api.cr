8021fa32677774211d84c91224385d076d0ad2c3
Daniel Zambrano <cacique.coder@gmail.com>
2021-06-02 09:38:39 +1000

    no changes, maybe trying with on_load methods

--- drivers/wcp/staff_api.cr
+++ drivers/wcp/staff_api.cr
@@ -5,4 +5,11 @@ require "oauth2"
 require "placeos"
 
 class Wcp::StaffAPI < PlaceOS::Driver
+  def on_load
+    logger.debug { "Do nothing" }
+  end
+
+  def on_update
+    logger.debug { "Do nothing" }
+  end
 end
